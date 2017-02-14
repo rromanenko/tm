7cadb673f8e6fb1e8cf934362a59879853dc60d9
Roman Romanenko <twaresare@gmail.com>
2017-02-14 16:37:30 +0300

    some experimentations to open a non-ascii file

--- tm.py
+++ tm.py
@@ -1,12 +1,25 @@
-# coding: Cyrillic
 #!/usr/bin/env python3
 
+''' old version
+# coding: Cyrillic
 import codecs
 
-fin = codecs.open('План работы.txt',encoding='cp1251')
+fin = codecs.open("План работы.txt",encoding='cp1251')
+''''''''''
 
-line = fin.readline().split()
+f = open("План работы.txt", "r",encoding="cp1251")
+
+line = f.readlines()
+
+#for i in line:
+#    print (i)
+
 print(line)
+
+f.close()
+
+#line = fin.readline().split()
+#print(line)
 
 # if valid_date
 # while line in fin:
