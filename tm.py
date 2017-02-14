a7b2cfb6deb53edc46f9cb0251c09af5ea9311e2
Roman Romanenko <twaresare@gmail.com>
2017-02-14 16:41:42 +0300

    setting up everything for work

--- tm.py
+++ tm.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
-''' old version
-# coding: Cyrillic
-import codecs
-
-fin = codecs.open("План работы.txt",encoding='cp1251')
-''''''''''
-
 f = open("План работы.txt", "r",encoding="cp1251")
 
 line = f.readlines()
-
-#for i in line:
-#    print (i)
-
 print(line)
 
 f.close()
-
-#line = fin.readline().split()
-#print(line)
 
 # if valid_date
 # while line in fin:
