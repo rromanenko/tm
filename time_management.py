301c05d585cb6dd7359866aa8a0957438e36a481
Roman Romanenko <twaresare@gmail.com>
2018-09-04 22:47:05 +0300

    tm script can work with en categories

--- time_management.py
+++ time_management.py
@@ -9,10 +9,6 @@ O  = '\033[33m' # orange
 B  = '\033[34m' # blue
 P  = '\033[35m' # purple
 
-#def valid_date(s):
-#    """ Checks if string is the beginning of the day, e.g. 27.02.2017, THURSDAY"""
-#    return "DAY" in s
-
 def valid_time(s):
     """ Checks if string is a time record, e.g. 01.55 - 02.10 - Б -
     If yes, returns a tuple with category and length of time in min
@@ -25,9 +21,8 @@ def valid_time(s):
     except ValueError:
         return ()
 
-f = open("/Volumes/untitled/План работы.txt", "r",encoding="cp1251")
+f = open("/Volumes/untitled/План работы.txt", "r", encoding="cp1251")
 in_date = False
-#categories = {}
 
 while True:
     line = f.readline()
@@ -42,10 +37,10 @@ while True:
         categories = {}
 
     elif in_date and valid_time(line): # if we are inside a date and line is a line with time
-        temp = valid_time(line) 
+        category, duration = valid_time(line) 
 
         # check for not 24h00 error. If not
-        # xx.xx- xx.15
+        # xx.xx - xx.15
         # xx.15 - xx.xx
         # print the line
         if prev_time != "" and line[3:5] != prev_time:
@@ -53,19 +48,26 @@ while True:
             print(color+line, W)
         prev_time = line[11:13]
 
-        if temp[0] == DISPLAY_BREAKDOWN:
+        cat_ru = "БИРsХ"
+        cat_en = "BIDsN"
+        if category in cat_en:
+            category = cat_ru[cat_en.find(category)]
+    
+        if category in DISPLAY_BREAKDOWN:
             print(line.rstrip())
 
-        if temp[0] in categories:
-            categories[temp[0]] += temp[1]
+        if category in categories:
+            categories[category] += duration
         else:
-            categories[temp[0]] = temp[1]
+            categories[category] = duration
 
     elif in_date and line == "\n": # if end of report for one day
         for i in "БИРsХ":
             if i in categories:
 #                print(i, "%2d h %2d min / " %(divmod(categories[i],60)), end="")
                 print( round(categories[i] // 60 + categories[i] % 60 / 60, 4))
+            else:
+                print(0)
 
         # if total is not "24 h 0 min", then print Total in different color
         (hours, minutes) = divmod(sum(categories.values()),60)
