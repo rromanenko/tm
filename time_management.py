8cdb94723dddb53cdb8f9b2d914c7a82fcd0ceba
Roman Romanenko <twaresare@gmail.com>
2019-08-29 19:21:50 +0300

    Updated tm script with calories count

--- time_management.py
+++ time_management.py
@@ -1,6 +1,9 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-DISPLAY_BREAKDOWN = 'Х' # if you want to display some category
+DISPLAY_BREAKDOWN = 'ХN' # if you want to display some category
+#DISPLAY_BREAKDOWN = 'BБ' # if you want to display some category
+#DISPLAY_BREAKDOWN = 'ИI' # if you want to display some category
+#DISPLAY_BREAKDOWN = 'Р' # if you want to display some category
 
 W  = '\033[0m'  # white (normal)
 R  = '\033[31m' # red
@@ -9,15 +12,20 @@ O  = '\033[33m' # orange
 B  = '\033[34m' # blue
 P  = '\033[35m' # purple
 
+cat_ru = "БИРsХ"
+cat_en = "BIDsN"
+
 def valid_time(s):
     """ Checks if string is a time record, e.g. 01.55 - 02.10 - Б -
     If yes, returns a tuple with category and length of time in min
     """
-    try: 
+    try:
         start_time = int(s[0:2])*60 + int(s[3:5])
         end_time = int(s[8:10])*60 + int(s[11:13])
         category = s[16:17]
-        return (category, end_time - start_time)
+        if category in (cat_en + cat_ru):
+            return (category, end_time - start_time)
+        return ()
     except ValueError:
         return ()
 
@@ -27,17 +35,22 @@ in_date = False
 while True:
     line = f.readline()
 
-    if '===' in line: # if end of all daily reports
+    if '=====' in line: # if end of all daily reports
         break
+
+    elif "kcal" in line: # if there's a number of eaten calories in the line
+        line = line.rstrip().split(" ")
+        total_cal += [int(line[line.index('kcal')-1] )]
 
     elif "DAY" in line: # if it's a start of a day, e.g. 27.02.2017, THURSDAY"""
         print("\n"+B+line+W)
         in_date = True
         prev_time = ""
         categories = {}
+        total_cal = []
 
     elif in_date and valid_time(line): # if we are inside a date and line is a line with time
-        category, duration = valid_time(line) 
+        category, duration = valid_time(line)
 
         # check for not 24h00 error. If not
         # xx.xx - xx.15
@@ -48,8 +61,6 @@ while True:
             print(color+line, W)
         prev_time = line[11:13]
 
-        cat_ru = "БИРsХ"
-        cat_en = "BIDsN"
         if category in cat_en:
             category = cat_ru[cat_en.find(category)]
     
@@ -76,6 +87,8 @@ while True:
         else:
             color = R
         print( color+"Total:", "%d h %d min" %(hours, minutes), W )
+        color = P
+        print(color+"Calories for the day:", total_cal, sum(total_cal))
         in_date = False
         
 f.close()
