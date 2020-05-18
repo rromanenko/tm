1a06bb49ae14afa8458ac61cdd90105cd12cda5e
Roman Romanenko <roman.romanenko@ix.co>
2020-05-18 16:11:10 +0300

    Updated tm script according to PEP8 warnings

--- time_management.py
+++ time_management.py
@@ -2,22 +2,24 @@
 
 import pyperclip
 
-DISPLAY_BREAKDOWN = 'ХN' # if you want to display some category
-#DISPLAY_BREAKDOWN = 'BБ' # if you want to display some category
-#DISPLAY_BREAKDOWN = 'ИI' # if you want to display some category
-#DISPLAY_BREAKDOWN = 'H' # if you want to display some category
+# choose what category to display
+DISPLAY_BREAKDOWN = 'ХN'
+# DISPLAY_BREAKDOWN = 'BБ'
+# DISPLAY_BREAKDOWN = 'ИI'
 
-White   = '\033[0m'
-Red     = '\033[31m'
-Green   = '\033[32m'
-Orange  = '\033[33m'
-Blue    = '\033[34m'
-Purple  = '\033[35m'
+
+White = '\033[0m'
+Red = '\033[31m'
+Green = '\033[32m'
+Orange = '\033[33m'
+Blue = '\033[34m'
+Purple = '\033[35m'
 
 cat_ru = "БИРсХ"
 cat_en = "BIDsN"
 
-metrics_list = ["touched myself", "push-up", "pull-up", "yoga"]
+metrics_list = ["touched myself", "push-up", "pull-up", "yoga", "eye exercise"]
+
 
 def valid_time(s):
     """ Checks if string is a time record, e.g. 01.55 - 02.10 - Б -
@@ -28,12 +30,13 @@ def valid_time(s):
         end_time = int(s[8:10])*60 + int(s[11:13])
         category = s[16:17]
         if category in (cat_en + cat_ru):
-            return (category, end_time - start_time)
+            return category, end_time - start_time
         return ()
     except ValueError:
         return ()
 
-#try to open the time management file, first on Mac, then on Windows
+
+# try to open the time management file, first on Mac, then on Windows
 try:
     path = "/Volumes/untitled/План работы.txt"
     f = open(path, "r", encoding="cp1251")
@@ -65,12 +68,12 @@ while True:
         prev_time = ""
         categories = {}
         total_cal = []
-        metrics = { i: 0 for i in metrics_list }
+        metrics = {i: 0 for i in metrics_list}
 
     # if there's a number of eaten calories in the line
     elif "kcal" in line:
         line = line.rstrip().replace(".", " ").split()
-        total_cal += [int(line[line.index('kcal')-1] )]
+        total_cal += [int(line[line.index('kcal')-1])]
 
     # if we are inside a date and line is a line with time
     elif in_date and valid_time(line):
@@ -91,30 +94,29 @@ while True:
         if category in DISPLAY_BREAKDOWN:
             print(line.rstrip())
 
-        # if category in categories:
-        #     categories[category] += duration
-        # else:
-        #     categories[category] = duration
+        # check if category is already added to categories dict, if not, create it and add duration as 0
+        # then increase its duration
         categories.setdefault(category, 0)
         categories[category] += duration
 
     # if end of report for one day, print time for each category
     elif in_date and line == "\n":
-        daily_results = "\n".join([str( round(categories.get(i, 0) // 60 + categories.get(i, 0) % 60 / 60, 4) ) for i in cat_ru])
+        daily_results = "\n".join([str(round(categories.get(i, 0) // 60 + categories.get(i, 0) % 60 / 60, 4))
+                                   for i in cat_ru])
         print(daily_results)
 
         # saving daily results for each category into computer clipboard
         pyperclip.copy(daily_results)
 
         # if total is not "24 h 0 min", then print Total in different color
-        (hours, minutes) = divmod(sum(categories.values()),60)
+        (hours, minutes) = divmod(sum(categories.values()), 60)
         if (hours, minutes) == (24, 0):
-            print( Green + "Total:", f"{hours} h", White )
+            print(Green + "Total:", f"{hours} h", White)
         else:
-            print( Red + "Total:", f"{hours} h {minutes} min", White )
+            print(Red + "Total:", f"{hours} h {minutes} min", White)
 
-        print( Purple + "Metrics: ", metrics )
-        #print(Purple+"Calories for the day:", total_cal, sum(total_cal))
+        print(Purple + "Metrics: ", metrics)
+        # print(Purple+"Calories for the day:", total_cal, sum(total_cal))
         in_date = False
         
 f.close()
