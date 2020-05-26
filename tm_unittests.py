d9e5016b7826a21ab67e895f34004f30bc36c2cd
Roman Romanenko <roman.romanenko@ix.co>
2020-05-26 10:41:42 +0300

    Unit tests

--- tm_unittests.py
+++ tm_unittests.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 27 01:38:57 2017
 
-@author: romanromanenko
-"""
-
-from time_management import valid_date, valid_time
+from time_management import valid_time
 import unittest
 
-class TMTest(unittest.TestCase):
+class TimeManagementTest(unittest.TestCase):
     def setUp(self):
         self.rand_text = 'SOME other text here'
     
-    def test_valid_date(self):
-        self.assertEqual( valid_date('THURSDAY'), 'THURSDAY' )
-        self.assertEqual( valid_date(self.rand_text), '' )
-
     def test_valid_time(self):
         self.assertEqual( valid_time('01.55 - 02.10 - Б -'), ('Б', 15) )
-        self.assertEqual( valid_time(self.rand_text), () )
+        self.assertEqual( valid_time('01.55 - 02.10 -'), () )
+        self.assertEqual( valid_time('archived it. Set goals'), () )
 
-#assertTrue, assertRaises
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
